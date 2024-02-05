cbb2923f6018acdfa26c8bb2e52c8d21abad39fb
mufeed <90856433+mufeedahamed@users.noreply.github.com>
2024-02-05 22:21:16 +0400

    Update database.js

--- database.js
+++ database.js
@@ -3,6 +3,7 @@ var md5 = require('md5')
 
 const DBSOURCE = "db.sqlite"
 
+
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
         // Cannot open database
@@ -10,7 +11,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         throw err
     } else {
         console.log('Connected to the SQlite database.')
-        db.run(`CREATE TABLE IF NOT EXISTS customer (
+        db.run(`CREATE TABLE customer (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name text, 
             address text,
@@ -25,16 +26,13 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             timeStamp text
             )`, (err) => {
             if (err) {
-                // handle table creation error
-                console.error(err.message);
+                console.log(err);
             } else {
-                // table exists or has been created successfully
-                //var insert = 'INSERT INTO customer (name, address, email, dateOfBirth, gender, age, cardHolderName, cardNumber, expiryDate, cvv, timeStamp) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
-                //db.run(insert, ["Mufeed M A", "No 324/A Ra De Mel Road, Colombo", "lakith@gmail.com", "1991.02.25", "male", 28, "Mufeed M A",102445217895 ,"12/2022", 246, "2022-12-31 23:59:59"])
+                console.log("Deleted");
             }
         })
+        
     }
 })
 
 module.exports = db
-
